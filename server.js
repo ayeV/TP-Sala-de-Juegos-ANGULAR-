ab3c791e124dc6d0e58c54c23521f7aa8ecb5c7c
Ayelen Valdez <ayelenvaldez07@gmail.com>
2020-09-17 14:29:13 -0300

    Fix

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(__dirname + '/TP-Sala-de-Juegos-ANGULAR-'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res)
 {
 
-    res.sendFile(path.join(__dirname + '/TP-Sala-de-Juegos-ANGULAR-/src/index.html'));
+    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
 
 console.log('Build successful');
